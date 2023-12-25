fa2864fe1b9ad9b1938668f8b7c417c6832f7d09
7Yuki <notsmile@proton.me>
2023-12-25 18:20:03 -0500

    Added setting referer as was required for amd download to work and moved function to file as it was related to using the internet

--- main/internet.go
+++ main/internet.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"github.com/vbauerster/mpb/v8"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func downloadFileProgressBar(url string, outFile *os.File, bar *mpb.Bar) error {
 
-	// Download file
-	resp, err := http.Get(url)
+	var resp *http.Response
+	client := &http.Client{}
+	req, reqErr := http.NewRequest("GET", url, nil)
+
+	if reqErr != nil {
+		return reqErr
+	}
+
+	if strings.Contains(url, "amd.com") {
+		req.Header.Set("Referer", "https://www.amd.com/")
+	}
+	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
+
 	defer resp.Body.Close()
 
 	buffer := make([]byte, 1024)
@@ -31,4 +45,38 @@ func downloadFileProgressBar(url string, outFile *os.File, bar *mpb.Bar) error {
 		}
 	}
 	return err
+}
+
+func downloadDriver(version string, brand string, path string) error {
+
+	bar := createDynamicProgressBar(strings.ToUpper(brand), version)
+
+	fileName := fmt.Sprintf("%v/%v-%v-desktop-win10-win11-64bit-graphics-drivers.exe", path, brand, version)
+	var url string
+
+	if strings.ToLower(brand) == "nvidia" {
+		url = fmt.Sprintf("https://us.download.nvidia.com/Windows/%v/%[1]v-desktop-win10-win11-64bit-international-dch-whql.exe", version)
+	} else if strings.ToLower(brand) == "amd" {
+		url = fmt.Sprintf("https://drivers.amd.com/drivers/whql-amd-software-adrenalin-edition-%v-win10-win11-dec5-rdna.exe", version)
+	} else if strings.ToLower(brand) == "intel" {
+		//todo: implement intel driver download
+	} else {
+		url = "unknown brand"
+	}
+
+	outFile, fileErr := os.Create(fileName)
+	if fileErr != nil {
+		panic(fileErr)
+	}
+	defer outFile.Close()
+
+	fmt.Printf("Downloading to: %v via link: %v\n", path, url)
+
+	err := downloadFileProgressBar(url, outFile, bar)
+	if err != nil {
+		return errors.New("download went wrong")
+	}
+	bar.Wait()
+
+	return nil
 }
